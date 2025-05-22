428dd15fe883f90268c733faedc6a1530697ef6c
Seifii <sofiapetrova893@gmail.com>
2025-05-22 17:50:51 +0300

    3

--- main1.py
+++ main1.py
@@ -8,11 +8,23 @@
 #         k += 1
 #     print(k)
 #
+#
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
+# logging.debug("debug")
+# logging.info("info")
+# logging.warning("warning")
+# logging.error("error")
+# logging.critical("critical")
+
+# import logging
+# logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w")
+# logging.debug("debug")
+# logging.info("info")
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
-logging.debug("debug")
-logging.info("info")
-logging.warning("warning")
-logging.error("error")
-logging.critical("critical")
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", format="We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
+try:
+ print(10/0)
+except Exception:
+ logging.exception("Exception")
